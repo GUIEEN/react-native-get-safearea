932c92eda841f3b1893d3e7cd634b5aa112fb124
GUIEEN(Seungyong Kwak) <guieenoutis@gmail.com>
2022-03-18 07:41:34 +0900

    Defined getSafeArea func on ts with its type

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { NativeModules, Platform } from 'react-native';
+import { NativeModules, Platform, StatusBar } from 'react-native';
 
 const LINKING_ERROR =
   `The package 'react-native-get-safearea' doesn't seem to be linked. Make sure: \n\n` +
@@ -17,6 +17,26 @@ const GetSafearea = NativeModules.GetSafearea
       }
     );
 
-export function multiply(a: number, b: number): Promise<number> {
-  return GetSafearea.multiply(a, b);
+export type ISafeArea = {
+  top: number;
+  bottom: number;
+  left: number;
+  right: number;
+};
+
+export async function getSafeArea(): Promise<ISafeArea> {
+  if (Platform.OS === 'android') {
+    // FIXME: - Kotlin
+    return new Promise((resolve, _) => {
+      const dict: ISafeArea = {
+        top: StatusBar.currentHeight ?? 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+      };
+      resolve(dict);
+    });
+  }
+
+  return GetSafearea.getSafeArea();
 }
