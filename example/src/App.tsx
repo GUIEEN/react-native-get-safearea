3a88ad98b079465a6e408e7881d4d9624c79fa87
GUIEEN(Seungyong Kwak) <guieenoutis@gmail.com>
2022-03-18 07:41:44 +0900

    Updated example of usage

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,14 @@
 import * as React from 'react';
-
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply } from 'react-native-get-safearea';
+import { Button, StyleSheet, View } from 'react-native';
+import { getSafeArea } from 'react-native-get-safearea';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
-
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Button
+        title="Test getSafeArea"
+        onPress={() => getSafeArea().then((notch) => console.log(notch))}
+      />
     </View>
   );
 }
@@ -22,10 +18,5 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
   },
 });
